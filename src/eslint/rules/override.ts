c470a71896da1e42d74183d390fbaf9058309c51
Thibaud <9969161+vouill@users.noreply.github.com>
2025-01-30 16:58:34 +0100

    fix: update gitignore

---
+++ src/eslint/rules/override.ts
@@ -0,0 +1,11 @@
+export default [
+    {
+        rules: {
+            'no-process-env': 0,
+            'prefer-const': 1,
+            'prefer-destructuring': 1,
+            'prefer-spread': 1,
+            'arrow-body-style': 0,
+        },
+    },
+] as const;
