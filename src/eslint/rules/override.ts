d2977c781c83b9df957008d4415319d40ed4ac54
Thibaud <9969161+vouill@users.noreply.github.com>
2025-02-03 10:40:55 +0100

    fix: reorganize rules

--- src/eslint/rules/override.ts
+++ src/eslint/rules/override.ts
@@ -6,6 +6,11 @@ export default [
             'prefer-destructuring': 1,
             'prefer-spread': 1,
             'arrow-body-style': 0,
+            'react/display-name': 0,
+            'react/prop-types': 0,
+            'react/react-in-jsx-scope': 0,
+            '@typescript-eslint/no-unsafe-member-access': 'off',
+            '@typescript-eslint/no-unsafe-assignment': 'off',
         },
     },
 ] as const;
--- src/eslint/rules/react.ts
+++ src/eslint/rules/react.ts
@@ -6,13 +6,6 @@ export default [
     reactPlugin.configs.flat?.recommended,
     reactPlugin.configs.flat?.['jsx-runtime'],
     {
-        rules: {
-            'react/display-name': 0,
-            'react/prop-types': 0,
-            'react/react-in-jsx-scope': 0,
-        },
-    },
-    {
         files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
         plugins: {
             react: reactPlugin,
